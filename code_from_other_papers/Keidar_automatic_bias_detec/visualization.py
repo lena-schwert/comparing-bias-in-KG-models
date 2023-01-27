20a798bd10a762b72d87f0d8b28e3fef983e883a
Lena <mail@mail.com>
2023-01-27 13:21:02 +0100

    cleanup: remove other ignored diretories from remote as well

--- code_from_other_papers/Keidar_automatic_bias_detec/visualization.py
+++
@@ -1,7 +0,0 @@
-from matplotlib import pyplot as plt
-
-def preds_histogram(preds_df):
-    plt.hist([preds_df.pred,preds_df.true_tail], label=["pred","true"])
-    plt.legend()
-    plt.title("true & predicted tails")
-    plt.show()
--- code_from_other_papers/Safavi_codex/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='codex',
-      version='0.1',
-      description='A simple API to access CoDEX data',
-      license='MIT',
-      packages=['codex'],
-      zip_safe=False)
